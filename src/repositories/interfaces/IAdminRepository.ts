7db20fd1985413fe273b822b039b8f4e9cf2f0f1
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2022-01-18 20:07:21 -0300

    feat: add adminRepository interface

---
+++ src/repositories/interfaces/IAdminRepository.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import Admin from '../../entities/Admin';
+
+interface IAdminRepository {
+    findByEmail(email: string): Promise<boolean>;
+
+    saveAdmin(admin: Admin): Promise<boolean>;
+}
+
+export default IAdminRepository;
