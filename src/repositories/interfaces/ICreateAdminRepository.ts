393b31dd3c6512417f5d94ee84cdb11ebfb2ac03
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2022-01-21 19:10:22 -0300

    refactor: class and file name

---
+++ src/repositories/interfaces/ICreateAdminRepository.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+
+import Admin from '../../entities/Admin';
+
+interface ICreateAdminRepository {
+    findByEmail(email: string): Promise<boolean>;
+
+    saveAdmin(admin: Admin): Promise<boolean>;
+}
+
+export default ICreateAdminRepository;
