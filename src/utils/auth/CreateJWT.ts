319c121ef14413ccb9a2c4b74423b665f2ee725c
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2022-01-20 21:21:33 -0300

    feat: add auth layer and createJWT class

---
+++ src/utils/auth/CreateJWT.ts
@@ -0,0 +1,49 @@
+import { sign, SignOptions, Algorithm } from 'jsonwebtoken';
+
+interface IPayloadJwt {
+    id: string;
+
+    name: string;
+
+    permissionLevel: number;
+}
+
+class CreateJwt {
+    private algorithmType: Algorithm = 'HS512';
+
+    private lifeCycle: string = process.env.JWT_LIFE_CYCLE;
+
+    private secretKey = process.env.JWT_SECRET;
+
+    private payload: IPayloadJwt;
+
+    private options: string;
+
+    constructor(payload: IPayloadJwt, options: string) {
+        this.payload = payload;
+
+        this.options = options;
+    }
+
+    public buildToken(): string {
+        const signOptions: SignOptions = {
+            algorithm: this.algorithmType,
+
+            expiresIn: this.lifeCycle,
+
+            subject: this.options,
+        };
+
+        const token: string = sign(
+            {
+                admin: this.payload,
+            },
+            this.secretKey,
+            signOptions
+        );
+
+        return token;
+    }
+}
+
+export default CreateJwt;
