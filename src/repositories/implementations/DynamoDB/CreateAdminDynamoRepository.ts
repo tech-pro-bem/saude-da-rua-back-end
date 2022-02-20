fbfa9e2f28838c7898eb3c36976835fe6a28f1e6
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2022-02-19 21:53:58 -0300

    fix: name validation

--- src/repositories/implementations/DynamoDB/CreateAdminDynamoRepository.ts
+++ src/repositories/implementations/DynamoDB/CreateAdminDynamoRepository.ts
@@ -1,4 +1,4 @@
-import { AWSError, DynamoDB } from 'aws-sdk';
+import { AWSError } from 'aws-sdk';
 import {
     DocumentClient,
     QueryInput,
@@ -9,12 +9,26 @@ import {
 } from 'aws-sdk/clients/dynamodb';
 import Admin from '../../../entities/Admin';
 import { ICreateAdminRepository } from '../../interfaces';
+import { DynamoDocumentClient } from './DynamoDocumentClient';
 
-export class CreateAdminDynamoRepository implements ICreateAdminRepository {
+export class CreateAdminDynamoRepository
+    extends DynamoDocumentClient
+    implements ICreateAdminRepository
+{
     private dynamoClientDB: DocumentClient;
 
     constructor() {
-        this.dynamoClientDB = new DynamoDB.DocumentClient();
+        super();
+        switch (process.env.IS_PROD) {
+            case 'true':
+                this.dynamoClientDB = super.getProdClient;
+                break;
+            case 'false':
+                this.dynamoClientDB = super.getDevClient;
+                break;
+            default:
+                this.dynamoClientDB = super.getJestClient;
+        }
     }
 
     public async checkIfAdminExistsByEmail(email: string): Promise<boolean> {
