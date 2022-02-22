f0980f4b8e364e0532cf7bbfcb1007488f8f97cf
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2022-02-22 19:58:26 -0300

    test: add volunteers table to offline test

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -16,5 +16,21 @@ module.exports = {
             ],
             BillingMode: 'PAY_PER_REQUEST',
         },
+        {
+            TableName: 'Volunteers-Table',
+            KeySchema: [
+                {
+                    AttributeName: 'email',
+                    KeyType: 'HASH',
+                },
+            ],
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'email',
+                    AttributeType: 'S',
+                },
+            ],
+            BillingMode: 'PAY_PER_REQUEST',
+        },
     ],
 };
