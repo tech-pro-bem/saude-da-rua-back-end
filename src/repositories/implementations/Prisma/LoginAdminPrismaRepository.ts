33a8cb9146a69fae58835b56ab03aebadd098702
antoniowellington542 <antoniowellington542@gmail.com>
2022-09-13 11:57:08 -0300

    feat: migrate Verify Volunteer to prisma

---
+++ src/repositories/implementations/Prisma/LoginAdminPrismaRepository.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import Admin from '../../../entities/Admin';
+import { PrismaPostgresClient } from '../../../helpers/database/PrismaPostgresClient';
+import { ILoginAdminRepository } from '../../interfaces';
+
+export class LoginAdminPrismaRepository
+    extends PrismaPostgresClient
+    implements ILoginAdminRepository
+{
+    private prisma: PrismaClient;
+
+    constructor() {
+        super();
+        this.prisma = super.getPrismaClient();
+    }
+
+    async getAdminInfoByEmail(email: string): Promise<Admin> {
+        const queryAdminParams = await this.prisma.ad
+    }
+}
