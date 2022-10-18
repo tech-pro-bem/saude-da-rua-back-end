0d13a8116a3134acf73004d4d384683a9d78bae8
Thalles Passos <contato@thalles.me>
2022-10-18 11:16:35 -0300

    feat: Add DTO

--- src/useCases/getAuthenticatedAdmin/GetAuthenticatedAdminDTO.ts
+++ src/useCases/getAuthenticatedAdmin/GetAuthenticatedAdminDTO.ts
@@ -2,4 +2,10 @@ type TGetAuthenticatedAdminDTO = {
     email: string;
 };
 
+type TGetAuthenticatedAdminResponseDTO = {
+   email: string;
+   id: string;
+   permissionLevel: string;
+};
+
 export default TGetAuthenticatedAdminDTO;
