918f79f0b4642f6a7d3f7d15e66c9469902bef96
Artur Straub <artur.straub@dito.com.br>
2022-11-21 18:06:37 -0300

    update seed file

--- src/helpers/seed/AdminPrismaSeed.ts
+++ src/helpers/seed/AdminPrismaSeed.ts
@@ -24,6 +24,7 @@ const prismaClient = prismaPostgres.getPrismaClient();
             email: 'test@example.com',
             name: 'test test',
             passwordHash,
+            permissionLevel: "1"
         }),
     });
 
