01ecec41adae68850c57aa85453a5beceb61d36a
Artur Straub <artur.straub@dito.com.br>
2022-11-30 14:06:21 -0300

    search by occupation

--- src/modules/volunteers/repositories/implementations/prisma/PrismaVolunteersRepository.ts
+++ src/modules/volunteers/repositories/implementations/prisma/PrismaVolunteersRepository.ts
@@ -97,7 +97,7 @@ export class PrismaVolunteersRepository
     }: GetVolunteersInput): Promise<Volunteer[]> {
         const parsedPage = page || 0
         const parsedLimit = limit || 20
-        const occupationKey = Object.keys(occupation).find(key => key.includes(searchTerm))
+        const occupationKey = Object.keys(occupation).find(key => key.toLowerCase().includes(searchTerm))
     
         const listOfVolunteers = await this.prisma.volunteer.findMany({
             orderBy: [
